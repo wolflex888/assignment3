0718a81fbc0b5611a884cd7574209d21a7b2546a
wolflex888 <wolflex888@gmail.com>
2022-03-12 18:47:10 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, Component} from 'react';
 import ReactDom from 'react-dom';
 import { render } from '@testing-library/react';
 
 
 function App() {
   const [comm, setComm] = useState([]);
-  // const inputRef = useRef(null);
-  // let val = inputRef.current.value;
+  const [formData, setFormData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
+  const inputRef = useRef([])
 
-
-  //  function getComments() {
-  //     fetch('/movie_comments', {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       }
-  //     }).then((response) => response.json())
-  // .then((data) => {
-  //   setComm(data.movie_comments);
-  // });
-  // }
+   async function updateComments() {
+      fetch('http://localhost:3001/movie_comments', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }).then((response) => response.json())
+  .then((data) => {
+    setComm(data.movie_comments);}
+    )
+  };
 
   useEffect(() => {
-    fetch('/movie_comments', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }).then((response) => response.json())
-      .then(data => {
-        setComm(data.movie_comments);
-      })
-  }, [])
+    updateComments()
+    .then(() => {
+      for (let i = 0; i < comm.length; i++){
+        inputRef.current[comm[i].comment_id] = {}
+        inputRef.current[comm[i].comment_id]["comment"] = comm[i].comment
+        inputRef.current[comm[i].comment_id]['rate'] = comm[i].rate
+      };
+      setIsLoading(false);
+    })
+  }, []);
 
 
   // const movie_info = comm.map(info => (
   //   <h3>{ info.rate }</h3>
   // ))
-  const movie_comment = comm.map(info => ((info.comment)))
-  const movie_rate = comm.map(info => ((info.rate)))
-
 
   // const [loading, setLoading] = useState(false);
 
@@ -73,7 +70,7 @@ function App() {
       method: "POST",
     }).then(res => res.json())
       .then(data => setComm(data.delete_comments));
-  }
+  };
 
   // const [Sav, setSav] = useState(null);
   // function saveComment() {
@@ -82,38 +79,54 @@ function App() {
   //   }).then(res => res.json())
   //     .then(data => setSav(data.data));
   // }
+  const handleCommentChange = (e) =>{
+  };
+  
 
+  const handleRateChange = (e) => {
 
-  function saveComments() {
-    console.log("goodbye")
+  };
+  
+  const handleChange = (e) => {
+    for (let i = 0; i < comm.length; i++){
+      if (comm[i].comment_id == e.target.id)
+      comm[i][e.target.name] = e.target.value 
+    };
+    console.log(inputRef.current)
+  };
+
+  function editComments(e) {
+    // fetch("http://localhost:3001/edit_comments", {
+    //   method: "PATCH",
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     formData
+    //   })})
+    console.log(inputRef.current)
+  };
+  if (isLoading){
+    return (<div className="App">Loading Data</div>)
   }
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Your reviews:</h1>
-        <div>
-          <p>Movie ID: 157336</p>
-          <input type="text" value={movie_rate[0]} name="rate1" onChange={deleteComments} />
-          <input type="text" value={movie_comment[0]} name="comment1" onChange={deleteComments} />
-          <button className="button"
-            onClick={deleteComments}>
-            delete
-          </button>
-        </div>
-        <div>
-          <p>Movie ID: 157336</p>
-          <input type="text" value={movie_rate[1]} name="rate2" onChange={deleteComments} />
-          <input type="text" value={movie_comment[1]} name="comment2" onChange={deleteComments} />
-          <button className="button"
-            onClick={deleteComments}>
-            delete
-          </button>
-        </div>
-        <button className="button"
-          onClick={saveComments}>
-          Save Changes
-        </button>
+          <form onSubmit={editComments}>
+            {comm && comm.map((item) => 
+            <div>
+              <label className='movie-id-tag'>Movie ID: 157336</label>
+              <input id={item.comment_id} ref={inputRef[item.comment_id]} name="rate" type="text" value={item.rate} onChange={handleChange}/>
+              <input id={item.comment_id} ref={inputRef[item.comment_id]} name="comment" type="text" value={item.comment} onChange={handleChange} />
+              <button id={item.comment_id} name="delete-button" type="submit">Delete</button>
+            </div>
+          )}
+          <button className="save-all-button" type="submit">Save Changes</button>
+          </form>
+
       </header>
     </div>
   );
